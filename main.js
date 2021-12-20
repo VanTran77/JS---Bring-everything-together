2f6d2886f8434aaa9e61f30c80e783662eab6085
Van Tran <tavan.it@gmail.com>
2021-12-21 00:00:42 +0100

    create table input

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var evenBtn = document.getElementById('even-btn');
 var oddBtn = document.getElementById('odd-btn');
 var showNums = document.getElementById('result')
+var firstName = document.getElementById('first-name');
+// var firstNameBtn = document.getElementById('first-name-btn');
+var lastName = document.getElementById('last-name');
+var fullNameBtn = document.getElementById('full-name-btn');
+
+var fullReport = document.getElementById('report');
+
 
 evenBtn.addEventListener("click", function(){
     // remove old value if we click again
@@ -27,4 +34,21 @@ function printNums(nums){
     newList.innerHTML = nums;
     // add new li to ul
     showNums.appendChild(newList);
+}
+
+// firstNameBtn.addEventListener("click", function(){
+//     //get text from input
+//     showNums.innerHTML = "";
+//     var fName = firstName.value;
+//     printNums(fName);
+// })
+// lastNameBtn.addEventListener("click", function(){
+//     //get text from input
+//     showNums.innerHTML = "";
+//     var lName = lastName.value;
+//     printNums(lName);
+// })
+
+function showFullName(){
+    console.log(firstName);
 }
