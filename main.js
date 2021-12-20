bca176edf21cee3d2cec95a6f4358651f09e0a0c
Van Tran <tavan.it@gmail.com>
2021-12-20 11:28:29 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let fName = document.getElementById("fn");
+let lName = document.getElementById("ln");
+let pwd = document.getElementById("pw");
+let eMail = document.getElementById("eml");
+let mSg = document.getElementById("msg");
+function clearFunc(){
+    document.getElementById("fn").value="";
+    document.getElementById("ln").value="";
+    document.getElementById("pw").value="";
+    document.getElementById("eml").value="";
+    document.getElementById("msg").value="";
+}
